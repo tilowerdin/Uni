e9871ea788430d87d4f9500cc3452d629de92af5
tilowerdin <werdin.tilo@arcor.de>
2018-01-28 10:56:16 +0100

    some unifikation and sld things

--- FortProgExercises/src/Uebung10/aufgabe3.pl
+++ FortProgExercises/src/Uebung10/aufgabe3.pl
@@ -6,10 +6,6 @@ mensch(schulz).
 in(X, [X|_]).
 in(X, [_|L]) :- in(X, L).
 
-gdw(X, Y) :-
-  (X,Y);
-  (not(X),not(Y)).
-
 bed1(V,K,S) :- not((in(meier, [V,K,S]), in(mueller, [V,K,S]))).
 
 bed2(V,K,S) :- not(in(mueller, [K,S])); V = schulz.
@@ -19,10 +15,6 @@ bed3(V,K,S) :- not(in(schroeder,[V,K,S])); in(meier, [V,K,S]).
 bed4(V,K,S) :- not(in(meier, [V,K,S])); not(S = schulz).
 
 bed5(V,K,S) :- not(in(schulz, [V,K,S])); not(V = schroeder).
-
-
-
-
 
 doit(V, K, S) :-
   mensch(V),
